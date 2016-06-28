e5215b1b76044d209d5b9f27cdd0e8eca2e8cbdf
Miri <kuroyujo@gmail.com>
2016-06-28 19:23:25 +0300

    two

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php 
+
 $leads = array();
 $leadOffset = 0;
 $tasks = array();
 $taskOffset = 0;
 
-$errors=array(
+
+
+$subdomain='test';
+function curlSetup($link)
+{
+  $errors=array(
     301=>'Moved permanently',
     400=>'Bad request',
     401=>'Unauthorized',
@@ -14,45 +20,6 @@ $errors=array(
     502=>'Bad gateway',
     503=>'Service unavailable'
   );
-
-$subdomain='test';
-
-//getting list of leads
-do {
-  $link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/list?limit_rows=500&limit_offset='.$leadOffset;
-  $curl=curl_init(); 
-  curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-  curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
-  curl_setopt($curl,CURLOPT_URL,$link);
-  curl_setopt($curl,CURLOPT_HEADER,false);
-  curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-  curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-  curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-  curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
-  $out=curl_exec($curl); 
-  $code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
-  curl_close($curl);
-  $code=(int)$code;
-  try
-  {
-    if($code!=200 && $code!=204)
-    throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error',$code);
-  }
-  catch(Exception $E)
-  {
-    die('Ошибка: '.$E->getMessage().PHP_EOL.'Код ошибки: '.$E->getCode());
-  }
-   
-  $Response=json_decode($out,true);
-
-  $leadsFromThisReq=$Response['response']['leads'];
-  $leads = array_merge($leads,$leadsFromThisReq);
-  $leadOffset += 500;
-} while(count($leadsFromThisReq)==500);
-
-
-do { 
-  $link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/tasks/list?type=lead&limit_rows=500&limit_offset='.$taskOffset;
   $curl=curl_init(); 
   curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
   curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
@@ -78,6 +45,24 @@ do {
   }
 
   $Response=json_decode($out,true);
+  return $Response;
+}
+
+//getting list of leads
+do {
+  $link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/list?limit_rows=500&limit_offset='.$leadOffset;
+
+  $Response = curlSetup($link);
+
+  $leadsFromThisReq=$Response['response']['leads'];
+  $leads = array_merge($leads,$leadsFromThisReq);
+  $leadOffset += 500;
+} while(count($leadsFromThisReq)==500);
+
+
+do { 
+  $link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/tasks/list?type=lead&limit_rows=500&limit_offset='.$taskOffset;
+  $Response = curlSetup($link);
   $tasksFromThisReq = $Response['response']['tasks'];
   $tasks = array_merge($tasks,$tasksFromThisReq);
   $taskOffset += 500;
